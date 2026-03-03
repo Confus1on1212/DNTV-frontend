39576d06f486b1958013547fb1a11e57b0742dc2
diak <diak@mesterlevel.lo>
2026-03-03 12:54:44 +0100

    fix color deprecation error and started isloggedin

--- src/Header.jsx
+++ src/Header.jsx
@@ -12,16 +12,18 @@ export default function Header() {
             <div className="container-fluid d-flex align-items-center justify-content-between">
                 <Link to={"/"} className="navbar-brand user-select-none text-decoration-none text-custom-yellow text-uppercase fw-bolder hover ms-3">DNTV</Link>
 
+                {isLoggedIn && 
                 <div className="navbar-nav nav-underline flex-row align-items-center overflow-x-auto">
                     <a className="nav-link text-custom-yellow user-select-none active" href="/">Home</a>
                     <a className="nav-link text-custom-blue user-select-none" href="#">Movies</a>
                     <a className="nav-link text-custom-blue user-select-none" href="#">Shows</a>
-                </div>
+                </div>}
 
+                {!isLoggedIn &&
                 <div className="d-flex">
                     <Link to={"/login"}><Btn btnClass={'btn btn-outline-custom-green mx-1'} content={'Log In'}/></Link>
                     <Link to={"/signup"}><Btn btnClass={'btn btn-outline-custom-yellow mx-1'} content={'Sign Up'}/></Link>
-                </div>
+                </div>}
             </div>
         </nav>
     )
